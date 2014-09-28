26425fdb32ec259df2b3f6a4f05739c6baae2d8c
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-09-28 08:56:59 +0200

    Fixed thread issue

--- main/qml/EmulatorItem.cpp
+++ main/qml/EmulatorItem.cpp
@@ -35,7 +35,7 @@ QSGNode *EmulatorItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *up
 	QSGSimpleTextureNode *n = static_cast<QSGSimpleTextureNode *>(oldNode);
 	if (!n) {
 		m_gl = window()->openglContext()->functions();
-		connect(window(), SIGNAL(frameFinished()), this, SLOT(onFrameFinished()), Qt::QueuedConnection);
+		connect(window(), SIGNAL(frameFinished()), this, SLOT(onFrameFinished()));
 		n = new QSGSimpleTextureNode();
 		QImage img(GFX_WIDTH, GFX_HEIGHT, QImage::Format_RGBA8888);
 		m_screen = new GLuint[GFX_WIDTH * GFX_HEIGHT];
@@ -46,6 +46,14 @@ QSGNode *EmulatorItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *up
 		n->setRect(boundingRect());
 	}
 
+	m_texture->bind();
+	m_gl->glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, GFX_WIDTH - 16, GFX_HEIGHT - 32, 0, GL_RGB, GL_UNSIGNED_BYTE, m_screen);
+
+	return n;
+}
+
+void EmulatorItem::onFrameFinished()
+{
 	QmlGeneratorWindow *win = qobject_cast<QmlGeneratorWindow *>(window());
 	uiplot_checkpalcache(0);
 	uiplot_convertdata32((uint8*)win->gfx(), m_screen, (320 + 16) * (240 + 16));
@@ -64,13 +72,6 @@ QSGNode *EmulatorItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *up
 		}
 		raw_src_screen += 32;
 	}
-	m_gl->glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, GFX_WIDTH - 16, GFX_HEIGHT - 32, 0, GL_RGB, GL_UNSIGNED_BYTE, m_screen);
-
-	return n;
-}
-
-void EmulatorItem::onFrameFinished()
-{
 	update();
 }
 
